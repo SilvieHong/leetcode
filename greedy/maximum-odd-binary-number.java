a75e8c27cd2cb9bf2dd48d51aa8f00793894b1a1
SilvieHong <122876238+SilvieHong@users.noreply.github.com>
2024-03-11 10:55:54 +0900

    Update maximum-odd-binary-number.java

--- greedy/maximum-odd-binary-number.java
+++ greedy/maximum-odd-binary-number.java
@@ -34,3 +34,28 @@ class Solution {
  Complexity:
  Time:O(n) and Space:O(n)
  */
+
+
+
+
+
+// https://leetcode.com/problems/maximum-odd-binary-number/solutions/4802354/beats-100-users-c-java-python-javascript-2-approaches-explained/
+class betterSolution {
+    public String maximumOddBinaryNumber(String s) {
+        int cnt1 = 0, cnt0 = 0;
+        for (char a : s.toCharArray()) {
+            if (a == '1') cnt1++;
+            else if (a == '0') cnt0++;
+        }
+        StringBuilder ans = new StringBuilder();
+        ans.append("1".repeat(cnt1 - 1));
+        ans.append("0".repeat(cnt0));
+        ans.append("1");
+        return ans.toString();
+    }
+}
+
+/**
+ Complexity:
+ Time:O(n) and Space:O(n)
+ */
