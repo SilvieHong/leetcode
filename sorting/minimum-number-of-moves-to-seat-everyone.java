6801c830c5966389b63c31df47a5e5fe4efbd15b
SilvieHong <122876238+SilvieHong@users.noreply.github.com>
2024-03-14 09:19:21 +0900

    Create minimum-number-of-moves-to-seat-everyone.java

---
+++ sorting/minimum-number-of-moves-to-seat-everyone.java
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/minimum-number-of-moves-to-seat-everyone/
+class Solution {
+    public int minMovesToSeat(int[] seats, int[] students) {
+        Arrays.sort(seats);
+        Arrays.sort(students);
+
+        int answer = 0;
+        for(int i=0; i<seats.length; i++){
+            answer += Math.abs(seats[i]-students[i]);
+        }
+
+        return answer;
+    }
+}
+
+/**
+ Complexity:
+ Time:O(nlogn) and Space:O(1)
+ */
