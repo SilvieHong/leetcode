a7f9bacb7b1292b8a5f53cb04b4fd318ad2ca9de
SilvieHong <122876238+SilvieHong@users.noreply.github.com>
2024-04-01 09:22:01 +0900

    Delete Database/not-boring-movies.sql

--- Database/not-boring-movies.sql
+++
@@ -1,5 +0,0 @@
--- https://leetcode.com/problems/not-boring-movies/?envType=study-plan-v2&envId=top-sql-50
-SELECT id, movie, description, rating
-FROM Cinema
-WHERE description != 'boring' AND id % 2 = 1
-ORDER BY rating DESC;
