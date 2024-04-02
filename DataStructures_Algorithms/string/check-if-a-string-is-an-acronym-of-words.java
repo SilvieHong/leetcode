f02703b60c5277c19c66aba836ba290972d95737
SilvieHong <122876238+SilvieHong@users.noreply.github.com>
2024-04-02 09:54:34 +0900

    Create check-if-a-string-is-an-acronym-of-words.java

---
+++ DataStructures_Algorithms/string/check-if-a-string-is-an-acronym-of-words.java
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/check-if-a-string-is-an-acronym-of-words/
+class Solution {
+    public boolean isAcronym(List<String> words, String s) {
+        if(words.size() != s.length()) return false;
+        
+        for (int i = 0; i < words.size(); i++) {
+            if (words.get(i).charAt(0) != s.charAt(i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+/**
+ * Complexity:
+ * Time:O(n) and Space:O(1)
+ */
