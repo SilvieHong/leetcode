75752eee74d19530ed36c94045b0bea9e4228097
SilvieHong <122876238+SilvieHong@users.noreply.github.com>
2024-04-02 09:51:53 +0900

    Create kids-with-the-greatest-number-of-candies.java

---
+++ DataStructures_Algorithms/array/kids-with-the-greatest-number-of-candies.java
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        List<Boolean> answer = new ArrayList<>();
+
+        int max = 0;
+        for(int i=0; i<candies.length; i++){
+            if(candies[i] > max) max = candies[i];
+        }
+
+        for(int i=0; i<candies.length; i++){
+            if(candies[i] + extraCandies >=max){
+                answer.add(true);
+            }else{
+                answer.add(false);
+            }
+        }
+        return answer;
+    }
+}
+
+/**
+ * Complexity:
+ * Time:O(n) and Space:O(n)
+ */
