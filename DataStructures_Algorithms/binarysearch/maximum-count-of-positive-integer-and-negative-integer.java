d523f6c2d6ec50984ee60385fd9d7dc3dd92c07e
SilvieHong <122876238+SilvieHong@users.noreply.github.com>
2024-04-10 15:25:08 +0900

    Update maximum-count-of-positive-integer-and-negative-integer.java

--- DataStructures_Algorithms/binarysearch/maximum-count-of-positive-integer-and-negative-integer.java
+++ DataStructures_Algorithms/binarysearch/maximum-count-of-positive-integer-and-negative-integer.java
@@ -39,3 +39,34 @@ class Solution {
  * Complexity:
  * Time:O(nlogn) and Space:O(1)
  */
+
+
+class betterSolution {
+    public int maximumCount(int[] nums) {
+        int n = nums.length;
+        int pos, neg;
+        neg = lowerBinary(nums, 0);
+        pos = n - lowerBinary(nums, 1);
+        return Math.max(pos, neg);
+    }
+
+    // find maximum index that equal or bigger than tar in nums array
+    private int lowerBinary(int[] nums, int tar){
+        int left = 0, right = nums.length - 1;
+        while (left <= right){
+            int mid = (left + right) / 2;
+            if(nums[mid] == tar){
+                right = mid - 1;
+            } else if(nums[mid] < tar){
+                left = mid + 1;
+            } else{
+                right = mid -1;
+            }
+        }
+        return left;
+    }
+}
+/**
+ * Complexity:
+ * Time:O(logn) and Space:O(1)
+ */
