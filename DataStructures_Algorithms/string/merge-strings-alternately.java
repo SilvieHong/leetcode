a604643001c4ecdbe5aff33a4ee3afaa56160cec
SilvieHong <122876238+SilvieHong@users.noreply.github.com>
2024-04-20 00:26:54 +0900

    Create merge-strings-alternately.java

---
+++ DataStructures_Algorithms/string/merge-strings-alternately.java
@@ -0,0 +1,35 @@
+//https://leetcode.com/problems/merge-strings-alternately/
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        StringBuilder sb = new StringBuilder();
+        char[] ch1 = word1.toCharArray();
+        char[] ch2 = word2.toCharArray();
+
+        int a = 0;
+        int b = 0;
+
+        for (int i = 0; i < Math.min(word1.length(), word2.length()) * 2; i++) {
+            if (i % 2 == 0) {
+                sb.append(ch1[a]);
+                a++;
+            } else {
+                sb.append(ch2[b]);
+                b++;
+            }
+        }
+
+        if(word1.length()== word2.length()){
+            return sb.toString();
+        }else if(word1.length() > word2.length()){
+            sb.append(word1.substring(word2.length()));
+        }else{
+            sb.append(word2.substring(word1.length()));
+        }
+        
+        return sb.toString();
+    }
+}
+/**
+ * Complexity:
+ * Time:O(min(word1.length(), word2.length()) and Space:O(n)
+ */
